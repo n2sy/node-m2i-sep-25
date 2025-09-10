6da2bccd8d570bb6df2243fb7bf14f40d74f464f
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-09-10 13:59:53 +0100

    add produitListe

---
+++ API-0/models/produit.model.js
@@ -0,0 +1,26 @@
+var produitListe = [
+  {
+    id: 1,
+    nom: "Produit 1",
+    prix: 312,
+    statut: "disponible",
+  },
+  {
+    id: 2,
+    nom: "Produit 2",
+    prix: 77,
+    statut: "indisponible",
+  },
+  {
+    id: 3,
+    nom: "Produit 3",
+    prix: 543,
+    statut: "epuise",
+  },
+  {
+    id: 4,
+    nom: "Produit 4",
+    prix: 400,
+    statut: "disponible",
+  },
+];
