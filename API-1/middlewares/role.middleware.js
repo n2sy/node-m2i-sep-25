c0a1ad59ace5593222e1809fe6f62fca2d219479
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2025-09-12 14:28:30 +0100

    add role Middleware

--- API-1/middlewares/role.middleware.js
+++ API-1/middlewares/role.middleware.js
@@ -1,6 +1,13 @@
-const isAdmin = (req, res, next) => {
-  console.log("Je suis le isAdmin Middleware");
-  next();
+const role = (roleDemande) => {
+  return (req, res, next) => {
+    if (req.roleUser !== roleDemande) {
+      let error = new Error("Vous n'avez pas les permissions nécessaires");
+      error.statusCode = 403;
+      next(error);
+    } else {
+      next();
+    }
+  };
 };
 
-module.exports = isAdmin;
+module.exports = role;
--- API-1/routes/book.routes.js
+++ API-1/routes/book.routes.js
@@ -9,15 +9,15 @@ const {
   searchBooks,
 } = require("../controllers/book.controller");
 const isAuth = require("../middlewares/auth.middleware");
-const isAdmin = require("../middlewares/role.middleware");
+const role = require("../middlewares/role.middleware");
 const router = express.Router();
 
 router.get("/", isAuth, getAllBooks);
-router.get("/search", searchBooks);
-router.get("/:id", getBookById);
-router.post("/add", addBook);
-router.put("/edit/:id", updateBook);
-router.delete("/delete/:id", deleteBook);
-router.patch("/restore/:id", restoreBook);
+router.get("/search", isAuth, searchBooks);
+router.get("/:id", isAuth, getBookById);
+router.post("/add", isAuth, role("admin"), addBook);
+router.put("/edit/:id", isAuth, role("admin"), updateBook);
+router.delete("/delete/:id", isAuth, role("admin"), deleteBook);
+router.patch("/restore/:id", isAuth, role("admin"), restoreBook);
 
 module.exports = router;
